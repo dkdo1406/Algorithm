bf784fc7ec604d9f63bffbbd45fc59f5f1593ca9
dkdo1406 <54826394+dkdo1406@users.noreply.github.com>
2022-10-04 19:58:31 +0900

    [Silver III] Title: 1로 만들기, Time: 668 ms, Memory: 39088 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,17 +1,14 @@
-import collections
 n = int(input())
-dp = collections.defaultdict(int)
 
+dp = [0 for i in range(n+1)]
 
-def make1(n):
-    if n==1:
-        return 0
-    for i in range(2,n+1):
-        dp[i] = dp[i - 1] + 1
-        if i%3==0 and dp[i]>dp[i//3]+1:
-            dp[i]=dp[i//3]+1
-        if i%2==0 and dp[i]>dp[i//2]+1:
-            dp[i] = dp[i // 2] + 1
+# 1일경우 횟수는 0이기 때문에 2부터 시작
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
 
-    return dp[n]
-print(make1(n))
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+
+print(dp[n])
