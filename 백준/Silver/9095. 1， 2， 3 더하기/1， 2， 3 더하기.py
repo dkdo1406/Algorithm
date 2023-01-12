0462f7a95b11419842312d27c384b9db795352b9
bereben <54826394+dkdo1406@users.noreply.github.com>
2023-01-12 16:12:21 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,22 +1,9 @@
-n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(int(input()))
-
-for m in arr:
-    arr = [0] * 4
-    if m < 4:
-        arr[0] = 0
-        arr[1] = 1
-        arr[2] = 2
-        arr[3] = 4
-        print(arr[m])
-        continue
-    arr = [0] * (m+1)
-    arr[0] = 0
-    arr[1] = 1
-    arr[2] = 2
-    arr[3] = 4
-    for i in range(4, m+1):
-        arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
-    print(arr[m])
+arr = [0] * 11
+arr[1] = 1
+arr[2] = 2
+arr[3] = 4
+for i in range(4, 11):
+    arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+T = int(input())
+for _ in range(T):
+    print(arr[int(input())])
