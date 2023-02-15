bb5917f6d091f192833d7c27b808751942f5764d
bereben <54826394+dkdo1406@users.noreply.github.com>
2023-02-15 15:34:04 +0900

    Time: 283 ms (90.19%), Space: 15.2 MB (36.53%) - LeetHub

--- LeetCode/Easy/0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ LeetCode/Easy/0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,12 +1,21 @@
 class Solution:
-    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        cnt = len(num) - 1
-        number = 0
-        for i in num:
-            number += i * (10 ** cnt)
-            cnt -= 1
-        ans = number + k
-        
-        ans_lst = list(map(int, str(ans)))
-        return ans_lst
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:  
+        num.reverse()
+        list_k = list(map(int, str(k)))
+        list_k.reverse()
+        cnt = 0
+        if len(num) < len(list_k):
+            num, list_k = list_k, num
+        while cnt < len(num):
+            if cnt < len(list_k):
+                num[cnt] += list_k[cnt]
+            if num[cnt] > 9:
+                num[cnt] -= 10
+                if len(num) <= cnt + 1:
+                    num.append(1)
+                else:
+                    num[cnt + 1] += 1
+            cnt += 1
+        num.reverse()
+        return num
             
