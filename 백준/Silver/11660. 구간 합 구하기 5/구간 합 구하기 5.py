fe81d5f1d499aeed5796db27c88967363125412e
bereben <54826394+dkdo1406@users.noreply.github.com>
2023-05-06 21:20:47 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1004 ms, Memory: 77100 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,30 @@
+import sys
+input = lambda : sys.stdin.readline()
+
+N, M = list(map(int, input().split()))
+
+ans = [0] * M
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+
+for r in range(N - 2, -1, -1):
+    graph[r][N - 1] += graph[r + 1][N - 1]
+    graph[N - 1][r] += graph[N - 1][r + 1]
+for r in range(N - 2, -1, -1):
+    for c in range(N - 2, -1, -1):
+        graph[r][c] += graph[r + 1][c] + graph[r][c + 1] - graph[r + 1][c + 1]
+
+for i in range(M):
+    r1, c1, r2, c2 = list(map(int, input().split()))
+    res = graph[r1-1][c1-1]
+    if r2 != N and c2 != N:
+        res += graph[r2][c2] - graph[r2][c1-1] - graph[r1-1][c2]
+    elif r2 != N:
+        res -= graph[r2][c1-1]
+    elif c2 != N:
+        res -= graph[r1-1][c2]
+    ans[i] = res
+
+for i in ans:
+    print(i)
