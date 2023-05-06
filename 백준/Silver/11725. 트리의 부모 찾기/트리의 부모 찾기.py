ed3e3ee7c9b08d3bac684a64f7b574d389970719
bereben <54826394+dkdo1406@users.noreply.github.com>
2023-05-06 16:42:40 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 4764 ms, Memory: 74956 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from collections import deque
+N = int(input())
+node_set = defaultdict(set)
+ans = [0] * (N + 1)
+
+for _ in range(N-1):
+    u, v = list(map(int, input().split()))
+    node_set[str(u)].add(v)
+    node_set[str(v)].add(u)
+q = deque()
+q.append(1)
+while q:
+    node = q.popleft()
+    if str(node) in node_set:
+        for n_node in node_set[str(node)]:
+            ans[n_node] = node
+            node_set[str(n_node)].remove(node)
+            q.append(n_node)
+
+for i in range(2, N+1):
+    print(ans[i])
