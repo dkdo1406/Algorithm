988d04ad75657b79b826a953eb293e8650f121ce
bereben <54826394+dkdo1406@users.noreply.github.com>
2023-08-06 17:35:54 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 29044 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -6,7 +6,10 @@ import java.io.*;
  *
  * N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오
  * 
- * 
+ * 구현
+ * ArrayDeque에 1부터 N까지의 값을 넣는다.
+ * 가장 앞에 값은 빼고, 그 다음 값은 가장 뒤에 넣는다.
+ * 이를 N - 1번 반복한다.
  * 
  */
 public class Main {
@@ -25,20 +28,25 @@ public class Main {
 		sb = new StringBuilder();
 		
 		N = Integer.parseInt(st.nextToken()); // 카드 수
-		int ans = 0;
 		
+		// 가장 앞에 값을 O(1)속도로 빼기 위해 deque자료구조를 사용
 		ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
 
+		// 1부터 N까지 값을 넣는다.
 		for(int idx = 1; idx < N + 1; idx++) {
 			deque.offer(idx);
 		}
 		
+		// 가장 앞에 값을 빼고, 그 다음 값을 제일 뒤로 넣는다.
 		for(int idx = 0; idx < N - 1; idx++) {
 			deque.poll();
 			deque.offer(deque.poll());
 		}
 
-		System.out.println(deque.poll());
+		// 마지막으로 남은 값을 뽑는다.
+		sb.append(deque.poll());
+		
+		System.out.println(sb);
 
 	}
 }
