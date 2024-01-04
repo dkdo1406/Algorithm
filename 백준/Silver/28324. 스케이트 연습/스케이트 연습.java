1e1fa22c91acd489a3588d59f118da0ba29a611d
bereben <54826394+dkdo1406@users.noreply.github.com>
2024-01-04 19:57:46 +0900

    [Silver IV] Title: 스케이트 연습, Time: 104 ms, Memory: 13004 KB, Score: 47 point -BaekjoonHub

---
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine().trim());
+        sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int speed[] = new int[N];
+        int ans = 0;
+
+        st = new StringTokenizer(br.readLine().trim());
+        for (int idx = 0; idx < N; idx++) {
+            // 각 구간 낼 수 있는 속력
+            speed[idx] = Integer.parseInt(st.nextToken());
+        }
+        // 도착속도는 0이다.
+        int currentSpeed = 0;
+        for (int idx = N - 1; idx >= 0; idx--) {
+            // 역순으로 확인
+            currentSpeed = Math.min(currentSpeed + 1, speed[idx]);
+            ans += currentSpeed;
+        }
+        System.out.println(ans);
+
+    }
+}
