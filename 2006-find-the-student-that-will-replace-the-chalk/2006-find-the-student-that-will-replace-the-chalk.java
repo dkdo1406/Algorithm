f5ada540d91399fbc30244ea83ac9dbebbe4f886
bereben <54826394+dkdo1406@users.noreply.github.com>
2024-09-02 17:20:08 +0900

    Time: 6 ms (26.67%), Space: 58.1 MB (93.58%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -1,13 +1,11 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        // 100,000 * 100,000
-        // 처음 10만개들 돌고 총합이 몇인지 확인한다.
+        // 처음 총합이 몇인지 확인한다.
         Long sum = 0L;
         for (int val : chalk) {
             sum += val;
         }
         k %= sum;
-        System.out.println(k + " " + sum + " " + chalk.length);
         // 그리고 k - idx한다.
         for (int idx = 0; idx < chalk.length; idx++) {
             k -= chalk[idx];
