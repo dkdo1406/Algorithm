cb54086ec69a81f99db6694a5a0e7d017b4fa87e
bereben <54826394+dkdo1406@users.noreply.github.com>
2024-09-05 16:20:03 +0900

    Time: 6 ms (23.61%), Space: 60.1 MB (66.23%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.java
+++ 2155-find-missing-observations/2155-find-missing-observations.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] missingRolls(int[] rolls, int mean, int n) {
         int m = rolls.length;
-        int sum = 0;
+        int isPossible = (mean * (n + m));
         for (int val : rolls) {
-            sum += val;
+            isPossible -= val;
         }
         // 가능한지 확인
-        int isPossible = (mean * (n + m)) - sum;
         if (isPossible > 6 * n || isPossible / n == 0 || isPossible <= 0) return new int[0];
         int[] res = new int[n];
         for (int idx = 0; idx < n; idx++) {
