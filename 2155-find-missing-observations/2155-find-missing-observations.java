3ee7d7bbb400acd9cda548b26a5d02ab0d610876
bereben <54826394+dkdo1406@users.noreply.github.com>
2024-09-05 16:24:38 +0900

    Time: 6 ms (23.61%), Space: 59.4 MB (86.89%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.java
+++ 2155-find-missing-observations/2155-find-missing-observations.java
@@ -7,7 +7,7 @@ class Solution {
         }
         // 가능한지 확인
         int isPossible = (mean * (n + m)) - sum;
-        if (isPossible > 6 * n || isPossible / n == 0 || isPossible <= 0) return new int[0];
+        if (isPossible > 6 * n || isPossible < n) return new int[0];
         int[] res = new int[n];
         for (int idx = 0; idx < n; idx++) {
             res[idx] = isPossible / (n - idx);
